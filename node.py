120e42857cec99dd4bbbc9196e2d1074cab30e58
tickten <tickten@outlook.com>
2024-03-18 03:34:49 +0800

    1

--- node.py
+++ node.py
@@ -1,42 +1,93 @@
-# from .typed import TypedClass
-from typed import *
+class ClipTextEncoder:
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "clip": ("CLIP",),
+                "text": (
+                    "STRING",
+                    {"multiline": True, "default": "Input your prompt."},
+                ),
+            },
+        }
+
+    RETURN_TYPES = (
+        "CLIP",
+        "CONDITIONING",
+    )
+    RETURN_NAMES = (
+        "clip",
+        "cond_out",
+    )
+    FUNCTION = "solve"
+    CATEGORY = "Tickt"
+
+    def encode(self, clip, text):
+        tokens = clip.tokenize(text)
+        cond, pooled = clip.encode_from_tokens(tokens, return_pooled=True)
+        return [[cond, {"pooled_output": pooled}]]
+
+    def solve(self, clip, text: str):
+        return (clip, self.encode(clip, text))
 
 
 class ClipTextEncoderPlus:
 
     @classmethod
-    def INPUT_TYPES(cls):
+    def INPUT_TYPES(s):
         return {
             "required": {
                 "clip": ("CLIP",),
-                "text": ("STRING", {"multiline": False}),
-                "cond": ("CONDITIONING",),
-                "opt": (["combine", "average"],),
+                "text": (
+                    "STRING",
+                    {"multiline": True, "default": "Input your prompt."},
+                ),
+                "cond_in": ("CONDITIONING",),
+                "opt": (["combine", "average", "ignore"],),
             },
         }
 
-    RETURN_TYPES = ("CONDITIONING",)
-    RETURN_NAMES = ()
+    RETURN_TYPES = (
+        "CLIP",
+        "CONDITIONING",
+    )
+    RETURN_NAMES = (
+        "clip",
+        "cond_out",
+    )
     FUNCTION = "solve"
-    CATEGORY = "TT"
+    CATEGORY = "Tickt"
 
     def encode(self, clip, text):
         tokens = clip.tokenize(text)
         cond, pooled = clip.encode_from_tokens(tokens, return_pooled=True)
-        return ([[cond, {"pooled_output": pooled}]],)
+        return [[cond, {"pooled_output": pooled}]]
 
-    def solve(self, clip, text: str, cond, opt: str):
+    def solve(self, clip, text: str, cond_in, opt: str):
         match opt:
             case "combine":
-                return [cond + self.encode(clip, text)]
-            case "average":
-                pass
+                return (
+                    clip,
+                    cond_in + self.encode(clip, text),
+                )
+            case "ignore":
+                return (
+                    clip,
+                    self.encode(clip, text),
+                )
+            case _:
+                raise NotImplemented
 
 
-NODE_CLASS_MAPPINGS: T_NODE_CLASS_MAPPINGS = {
-    "ClipTextEncoderPlus": ClipTextEncoderPlus
+NODE_CLASS_MAPPINGS = {
+    "ClipTextEncoder": ClipTextEncoder,
+    "ClipTextEncoderPlus": ClipTextEncoderPlus,
 }
-NODE_DISPLAY_NAME_MAPPINGS = {"ClipTextEncoderPlus": "Clip Text Encoder +"}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ClipTextEncoder": "Clip Text Encoder",
+    "ClipTextEncoderPlus": "Clip Text Encoder +",
+}
 
 if __name__ == "__main__":
     """"""
